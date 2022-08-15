b63b54f223dd9cfb7b83c2cd7a09be77820a22d8
Abhinab Shrestha <abhinabshrestha9@gmail.com>
2022-08-15 17:17:15 +0545

    parenthesis

---
+++ parenthesis.cpp
@@ -0,0 +1,18 @@
+//multiple parenthesis
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+struct stack{
+	int top;
+	int size;
+	char arr*;
+	
+};
+int main(){
+	struct stack *s=(struct stack*)malloc(sizeof(struct stack));
+int a,b,c,d,p;
+s->top=-1;
+s->size=80;
+s->arr=(int*)malloc(s->size*sizeof(int));
+}
