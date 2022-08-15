a3ecc9d734f431b39da793e8401c59493686a416
Abhinab Shrestha <abhinabshrestha9@gmail.com>
2022-08-15 17:59:03 +0545

    second change

--- parenthesis.cpp
+++ parenthesis.cpp
@@ -9,10 +9,29 @@ struct stack{
 	char arr*;
 	
 };
-int main(){
+int parenthesisMatch(char* exp){
+
 	struct stack *s=(struct stack*)malloc(sizeof(struct stack));
 int a,b,c,d,p;
 s->top=-1;
 s->size=80;
 s->arr=(int*)malloc(s->size*sizeof(int));
+char popped_ch;
+for (int i=0; exp[i]!='\0';i++){
+	if(exp[i]=='('|| exp[i]=='{' || exp=='[')
+	{
+		push (sp,exp[i]);
+	}
+	else if(exp[i]==')'){
+		if(isEmpty(sp)){
+			return 0;
+		}
+	popped_ch=	pop(sp);
+	if(!match(popped_ch, stackTop(sp)))
+	{
+		return 0;
+	}
+	}
+}
+
 }
