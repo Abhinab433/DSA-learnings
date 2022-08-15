a237d19925fe40ebdb06c1d7c8c4896b040216d8
Abhinab Shrestha <abhinabshrestha9@gmail.com>
2022-08-15 16:52:54 +0545

    precedence function

---
+++ Untitled1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+
+
+char precedence(char b){
+	if(b=='^'){
+		return 1;
+	}
+	else if(b=='/' || b=='*'){
+		return 2;
+	}
+	else if(b=='+' || b=='-'){
+		return 3;
+	}
+	else if 
+}
+int main(){
+	
+}
